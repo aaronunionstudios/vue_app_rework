a0d1f422c7a745513b6f75f06b496947f33fea8b
Aaron Wilson <aaron@unionstudios.com>
2021-12-16 15:23:24 -0500

    adding in v-bind

--- script.js
+++ script.js
@@ -2,18 +2,30 @@ new Vue({
     el: '#notebook',
     data () {
         return {
-            content: 'This is a note.',
-        }
-    },
-    computed: {
-        notePreview () {
-            return marked.parse(this.content)
+            // content: 'This is a note.',
+            content: localStorage.getItem('content') || 'You can write in **markdown**',
+            notes: []
         }
     },
     watch: {
         content: 'saveNote',
     },
+    created () {
+        this.content = localStorage.getItem('content') || 'You can write in **markdown**'
+    },
     methods: {
+        addNote () {
+            console.log('click')
+            const time = Date.now()
+            const note = {
+                id: String(time),
+                title: 'New note' + (this.notes.length + 1),
+                content: '**Hi!** This notebook is using [markdown] (https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet) for formatting!',
+                created: time,
+                favorite: false,
+            }
+            this.notes.push(note)
+        },
         saveNote () {
             console.log('saving note:', this.content)
             localStorage.setItem('content', this.content)
@@ -21,7 +33,16 @@ new Vue({
         },
         reportOperation (opName) {
             console.log('The', opName, 'operation was completed!')
+        },
+    },
+    computed: {
+        notePreview () {
+            return marked.parse(this.content)
+        },
+        addButtonTitle () {
+            console.log('fire')
+            return notes.length + 'note(s) already' 
         }
-    }
+    },
 })
 console.log('restored note:', localStorage.getItem('content'))
